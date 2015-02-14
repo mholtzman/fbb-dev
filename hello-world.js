867238b44ee64bac8d2eb9f5603b4d34b3141d2d
Matt Holtzman <matt.holtzman@gmail.com>
2015-02-14 17:02:39 -0500

    initial cut of POST service to add players to DB

--- hello-world.js
+++ hello-world.js
@@ -1,10 +1,71 @@
-var express = require("express");
+var express = require("express"),
+    parser = require("body-parser"),
+    aws = require("aws-sdk");
 
 var app = express();
 var port = process.env.PORT || 9080;
 
-app.get('/', function(req, resp) {
-    resp.end('Hello, world!');
+app.use(parser.json());
+
+app.use(function (req, res) {
+    var db = new aws.DynamoDB({accessKeyId: 'AKIAJM3ETS42UCTXZH3Q', secretAccessKey: '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', region: 'us-east-1'});
+
+    var params = {
+        TableName: 'players',
+        Item: {
+            player_id: {
+                S: req.body.player_id
+            },
+            first_name: {
+                S: req.body.first_name
+            },
+            last_name: {
+                S: req.body.last_name
+            },
+            position: {
+                S: req.body.position
+            },
+            projection: {
+                M: {
+                    hr: {
+                        N: req.body.projection.hr
+                    },
+                    rbi: {
+                        N: req.body.projection.avg
+                    },
+                    avg: {
+                        N: req.body.projection.avg
+                    }
+                }
+            }
+        }
+    };
+
+    db.deleteItem({
+        TableName: 'players',
+        Key: {
+            player_id: {
+                S: req.body.player_id
+            }
+        }
+    }, function(err, data) {
+        if (err) {
+            console.log(err);
+            res.end("error!");
+        }
+        else {
+            console.log("Player deleted successfully!");
+            db.putItem(params, function(err, data) {
+                if (err) {
+                    console.log(err);
+                    res.end("error adding player!");
+                }
+                else {
+                    res.end("Player added successfully!");
+                }
+            });
+        }
+    });
 });
 
 app.listen(port, function() {
