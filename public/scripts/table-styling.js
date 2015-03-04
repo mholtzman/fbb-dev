222b0043c1392b7b188b39416eb55e7a7f351056
Matt Holtzman <matt.holtzman@gmail.com>
2015-03-04 16:30:46 -0500

    Cached data on the server to improve performance

--- public/scripts/table-styling.js
+++ public/scripts/table-styling.js
@@ -9,14 +9,14 @@ var threeDecimals = function(data) {
 }
 
 var ops = function(row) {
-    var projection = row.projections;
+    var projection = row.projection;
 
     if (projection) {
         var obp = projection.obp;
         var slg = projection.slg;
 
         if (obp && slg) {
-            return Number(row.projections.obp) + Number(row.projections.slg);
+            return Number(row.projection.obp) + Number(row.projection.slg);
         }
     }
 
@@ -58,18 +58,18 @@ $(document).ready(function() {
             { name: "name", title: "Name", data: "name", searchable: true, orderable: false },
             { name: "team", title: "Team", data: "team", searchable: true, orderable: false },
             { name: "positions", title: "Positions", data: positions, searchable: true, orderable: false },
-            { name: "plate_appearances", title: "PA", data: "projections.pa", render: noDecimals },
-            { name: "at_bats", title: "AB", data: "projections.ab", render: noDecimals },
-            { name: "hits", title: "H", data: "projections.h", render: noDecimals },
-            { name: "runs", title: "R", data: "projections.r", render: noDecimals },
-            { name: "rbi", title: "RBI", data: "projections.rbi", render: noDecimals },
-            { name: "home_runs", title: "HR", data: "projections.hr", render: noDecimals },
-            { name: "stolen_bases", title: "SB", data: "projections.sb", render: noDecimals },
-            { name: "caught_stealing", title: "CS", data: "projections.cs", render: noDecimals },    
-            { name: "strikeouts", title: "K", data: "projections.k", render: noDecimals },
-            { name: "batting_average", title: "AVG", data: "projections.avg", render: threeDecimals },
-            { name: "on_base_pct", title: "OBP", data: "projections.obp", render: threeDecimals },
-            { name: "slugging_pct", title: "SLG", data: "projections.slg", render: threeDecimals },
+            { name: "plate_appearances", title: "PA", data: "projection.pa", render: noDecimals },
+            { name: "at_bats", title: "AB", data: "projection.ab", render: noDecimals },
+            { name: "hits", title: "H", data: "projection.h", render: noDecimals },
+            { name: "runs", title: "R", data: "projection.r", render: noDecimals },
+            { name: "rbi", title: "RBI", data: "projection.rbi", render: noDecimals },
+            { name: "home_runs", title: "HR", data: "projection.hr", render: noDecimals },
+            { name: "stolen_bases", title: "SB", data: "projection.sb", render: noDecimals },
+            { name: "caught_stealing", title: "CS", data: "projection.cs", render: noDecimals },    
+            { name: "strikeouts", title: "K", data: "projection.k", render: noDecimals },
+            { name: "batting_average", title: "AVG", data: "projection.avg", render: threeDecimals },
+            { name: "on_base_pct", title: "OBP", data: "projection.obp", render: threeDecimals },
+            { name: "slugging_pct", title: "SLG", data: "projection.slg", render: threeDecimals },
             { name: "ops", title: "OPS", data: ops, render: threeDecimals }
         ];
 
--- routes/projections.js
+++ routes/projections.js
@@ -1,6 +1,7 @@
 var express = require('express'),
     aws = require('aws-sdk'),
-    _ = require('underscore');
+    _ = require('underscore'),
+    Q = require('q');
 
 var router = express.Router();
 
@@ -22,15 +23,17 @@ function aggregateProjections(projections) {
             .value();
 }
 
-router.get('/', function(req, resp) {
-    var sites = _.flatten([req.query.site]);
-    var db = new aws.DynamoDB({ region: 'us-east-1' });
-    var allPlayerData = {};
+var allPlayerData = {};
+
+var db = new aws.DynamoDB({ region: 'us-east-1' });
+
+var loadPlayerData = function() {
+    var load = Q.defer();
 
     db.scan({ TableName: "players" }, function(err, data) {
         if (err) {
             console.error("couldn't load any player data!");
-            resp.end();
+            load.reject();
         }
         else {
             data.Items.forEach(function(player) {
@@ -48,40 +51,70 @@ router.get('/', function(req, resp) {
                 allPlayerData[player.player_id.S] = newPlayer;
             });
 
-            db.scan({ TableName: "projections" }, function(err, data) {
-                if (err) {
-                    console.error("error loading projections: ");
-                    resp.end();
-                } else {
-                    var responseData = [];
-
-                    // join the player data with the projection data
-                    data.Items.forEach(function(projectionData) {
-                        var playerId = projectionData.player_id.S;
-                        var player = allPlayerData[playerId];
-
-                        // first, get the set of requested projections
-                        var requestedProjectionTypes = _.intersection(sites, _.keys(projectionData));
-
-                        // then pull out the requested projections
-                        var requestedProjections =
-                            _.chain(projectionData)
-                            .pick(requestedProjectionTypes)
-                            .map(function(val) {
-                                return val.M;
-                            }).value();
-
-                        // then aggregate all the data
-                        player.projections = aggregateProjections(requestedProjections);
-
-                        responseData.push(player);
-                    });
-
-                    resp.status(200).json({ data: responseData });
-                }
-            });
+            load.resolve();
         }
     });
+
+    return load.promise;
+};
+
+var loadProjectionData = function() {
+    var load = Q.defer();
+
+    db.scan({ TableName: "projections" }, function(err, data) {
+        if (err) {
+            console.error("error loading projections: ");
+            load.reject();
+        } else {
+            // join the player data with the projection data
+            data.Items.forEach(function(projectionData) {
+                var playerId = projectionData.player_id.S;
+                allPlayerData[playerId].projections = _.omit(projectionData, 'player_id');
+            });
+
+            load.resolve();
+        }
+    });
+
+    return load.promise;
+};
+
+
+router.get('/', function(req, resp) {
+    var sites = _.flatten([req.query.site]);
+    var responseData = [];
+
+    for (var player_id in allPlayerData) {
+        var playerData = allPlayerData[player_id];
+
+        var response = {
+            name: playerData.name,
+            team: playerData.team,
+            positions: playerData.positions,
+            projection: {}
+        };
+
+        // first, get the set of requested projections
+        var projectionData = playerData.projections;
+        var requestedProjectionTypes = _.intersection(sites, _.keys(projectionData));
+
+        // then pull out the requested projections
+        var requestedProjections =
+            _.chain(projectionData)
+            .pick(requestedProjectionTypes)
+            .map(function(val) {
+                return val.M;
+            }).value();
+
+        // then aggregate all the data
+        response.projection = aggregateProjections(requestedProjections);
+
+        responseData.push(response);
+    }
+
+    resp.status(200).json({ data: responseData });
 });
+
+loadPlayerData().then(loadProjectionData);
 
 module.exports = router;
