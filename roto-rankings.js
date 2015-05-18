431067a16961cbda33a2c781cb4ce186b91a874d
Matt Holtzman <holtzman@social-alpha.com>
2015-05-18 18:26:12 -0400

    Added parsing for standings data

--- roto-rankings.js
+++ roto-rankings.js
@@ -1,12 +1,32 @@
 var file = require('fs'),
     request = require('request'),
     Q = require('q'),
-    open = require('open');
+    open = require('open'),
+    cheerio = require('cheerio'),
+    _ = require('underscore');
 
-var loginToESPN = function() {
+function Team(name) {
+    this.name = name;
+
+    this.r = 0;
+    this.rbi = 0;
+    this.hr = 0;
+    this.sb = 0;
+    this.avg = 0;
+    this.ops = 0;
+
+    this.k = 0;
+    this.w = 0;
+    this.l = 0;
+    this.sv = 0;
+    this.era = 0;
+    this.whip = 0;
+
+    this.total = 0;
+}
+
+var loginToESPN = function(username, password) {
     var loginUrl = "https://r.espn.go.com/espn/memberservices/pc/login";
-    var username = "foxxxer";
-    var password = "x316F4zmFlTq";
 
     var formData = {
         failedAttempts: 0,
@@ -37,9 +57,9 @@ var loginToESPN = function() {
     });
 };
 
-loginToESPN();
+loginToESPN(process.argv[2], process.argv[3]);
 
-var openLeaguePage = function(outputFile, pitchers) {
+var openLeaguePage = function() {
     var leagueUrl = "http://games.espn.go.com/flb/standings?leagueId=128598&seasonId=2015";
     var options = {
         url: leagueUrl,
@@ -50,11 +70,9 @@ var openLeaguePage = function(outputFile, pitchers) {
         if (err) {
             console.error("error: " + err);
         } else {
-            console.log("status: " + response.statusCode + "\n\n" + JSON.stringify(body));
-            //file.writeFileSync("C:\\dev\\output.html", body);
-            //open('file:///C:\\dev\\output.html');
+            console.log("status: " + response.statusCode + "\n\n");
 
-            //openLeaguePage();
+            processStandings(body);
         }
     })
     /*var pageIndex = 0;
@@ -94,3 +112,93 @@ var openLeaguePage = function(outputFile, pitchers) {
         });
     }*/
 };
+
+function processStandings(html) {
+    var $ = cheerio.load(html);
+
+    var categories = [], teams = [];
+    var categoryRow = $('#statsTable .tableSubHead').last();
+    categoryRow.find('td a.hand').each(function() {
+        categories.push($(this).text().toLowerCase());
+    });
+
+    var userRows = $('#statsTable').find('.sortableRow');
+
+    userRows.each(function() {
+        var team = new Team($(this).find('.sortableTeamName a').first().text());
+        var stats = $(this).children('.precise');
+
+        for (var i = 0; i < categories.length; i++) {
+            team[categories[i]] = Number(stats.eq(i).text());
+        }
+
+        teams.push(team);
+    });
+
+    // sort and calculate totals
+    categories.forEach(function(category) {
+        var sortedList = _.sortBy(teams, category);
+        for (var i = 0; i < 12; i++) {
+            if (category === 'l' || category === 'era' || category === 'whip') {
+                sortedList[11 - i].total += (i + 1);
+            } else {
+                sortedList[i].total += (i + 1);
+            }
+        }
+    });
+
+    var standings = _.sortBy(teams, 'total');
+    for (var i = standings.length - 1; i >= 0; i--) {
+        console.log(standings[i].name + ": " + standings[i].total);
+    }
+
+
+    /*this.parse = function($, includeCategories) {
+        var parsedData = [];
+        var positionSelector = this.positionSelector;
+
+        var matchedPlayers = $('div.games-fullcol table').not(function() {
+            var playerData = $(this).find('.subheadPlayerNameLink').text();
+            var positions = _str.strRightBack(_str.replaceAll(playerData, ", ", ","), " ");
+            return !positionSelector.matches(positions);
+        });
+
+        if (includeCategories) {
+            var categories = ['player_id','first_name','last_name','team','positions'];
+
+            // scrape the categories
+            matchedPlayers.first().find('tr.tableSubHead td.playertableStat').each(function() {
+                categories.push($(this).text().toLowerCase());
+            });
+
+            parsedData.push(categories);
+        }
+
+        matchedPlayers.each(function() {
+            var player = [];
+
+            // first scrape player name, team, position
+            var playerData = $(this).find('.subheadPlayerNameLink').text();
+
+            // remove ranking info
+            var playerInfo = _str.words(_str.replaceAll(_str.strRight(playerData, ". "), ",", ""));
+            var firstName = playerInfo[0];
+            var lastName = playerInfo[1];
+
+            player.push(utils.getPlayerId(firstName, lastName));
+            player.push(firstName);
+            player.push(lastName);
+            player.push(playerInfo[2].toUpperCase()); // team
+            player.push(positions.normalize(_.rest(playerInfo, 3).join(","))); // positions
+
+            // then scrape stats
+            var stats = $(this).find('tr.tableBody').last().find('td.playertableStat').each(function() {
+                player.push($(this).text());
+            });
+
+            parsedData.push(player);
+        });
+
+        return parsedData;
+    }*/
+}
